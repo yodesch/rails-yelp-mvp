5c0dcce7f39e0957be7b2f9b91836d0d5ecaad65
Yoann DESCHAUX <yoanndeschaux@gmail.com>
2022-11-16 15:19:15 +0100

    yelp mvp version0

---
+++ db/migrate/20221116125512_add_id_restaurant_and_rating_to_review.rb
@@ -0,0 +1,5 @@
+class AddIdRestaurantAndRatingToReview < ActiveRecord::Migration[7.0]
+  def change
+    add_column :reviews, :rating, :integer
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Book.title,
+    address: Faker::Restaurant.description,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+  restaurant.save!
+end
